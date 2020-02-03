eecda7c9c2e34a6d56b951237ac49ff505f6ed5a
maxim van den eede <maximvde@gmail.com>
2020-02-03 14:27:18 +0100

    Initial commit

---
+++ WinLockPick/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WinLockPick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundPictureBox = new System.Windows.Forms.PictureBox();
+            this.accountImagePictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accountImagePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backgroundPictureBox
+            // 
+            this.backgroundPictureBox.Location = new System.Drawing.Point(-1, -1);
+            this.backgroundPictureBox.Name = "backgroundPictureBox";
+            this.backgroundPictureBox.Size = new System.Drawing.Size(801, 452);
+            this.backgroundPictureBox.TabIndex = 0;
+            this.backgroundPictureBox.TabStop = false;
+            // 
+            // accountImagePictureBox
+            // 
+            this.accountImagePictureBox.Location = new System.Drawing.Point(228, 88);
+            this.accountImagePictureBox.Name = "accountImagePictureBox";
+            this.accountImagePictureBox.Size = new System.Drawing.Size(291, 251);
+            this.accountImagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.accountImagePictureBox.TabIndex = 1;
+            this.accountImagePictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.accountImagePictureBox);
+            this.Controls.Add(this.backgroundPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.accountImagePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox backgroundPictureBox;
+        private System.Windows.Forms.PictureBox accountImagePictureBox;
+    }
+}
+
---
+++ WinLockPick/Helpers/ImageHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Drawing;
+using Microsoft.Win32;
+
+namespace WinLockPick.Helpers
+{
+    class ImageHelper
+    {
+        public static Bitmap GetLockscreenWallpaper()
+        {
+            string spotlightPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\localstate\assets");
+            string defaultWinPath = @"C:\Windows\Web\Wallpaper\Windows\img0.jpg";
+
+            if (Directory.Exists(spotlightPath) && Directory.GetFiles(spotlightPath).Any())
+                return new Bitmap(new DirectoryInfo(spotlightPath).GetFiles().OrderByDescending(f => f.CreationTime).ToArray()[0].FullName);  
+            else return new Bitmap(defaultWinPath);
+        }
+        public static Bitmap GetAccountImage()
+        {
+            //Special thanks to Keldnorman for this snippet
+
+            RegistryKey AccountPictureReg = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\AccountPicture", true);
+            string AccountPictureFilename = AccountPictureReg.GetValue("SourceId").ToString();
+            AccountPictureReg.Close();
+            var accPicture = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Microsoft\\Windows\\AccountPictures\\" + AccountPictureFilename + ".accountpicture-ms");
+
+            return new Bitmap(accPicture);
+        }
+        private static Bitmap GetImage448(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+            long position = Seek(fs, "JFIF", 100);
+            byte[] b = new byte[Convert.ToInt32(fs.Length)];
+            fs.Seek(position - 6, SeekOrigin.Begin);
+            fs.Read(b, 0, b.Length);
+            fs.Close();
+            fs.Dispose();
+            return GetBitmapImage(b);
+        }
+        private static long Seek(FileStream fs, string searchString, int startIndex)
+        {
+            char[] search = searchString.ToCharArray();
+            long result = -1, position = 0, stored = startIndex,
+            begin = fs.Position;
+            int c;
+            while ((c = fs.ReadByte()) != -1)
+            {
+                if ((char)c == search[position])
+                {
+                    if (stored == -1 && position > 0 && (char)c == search[0])
+                    {
+                        stored = fs.Position;
+                    }
+                    if (position + 1 == search.Length)
+                    {
+                        result = fs.Position - search.Length;
+                        fs.Position = result;
+                        break;
+                    }
+                    position++;
+                }
+                else if (stored > -1)
+                {
+                    fs.Position = stored + 1;
+                    position = 1;
+                    stored = -1;
+                }
+                else
+                {
+                    position = 0;
+                }
+            }
+
+            if (result == -1)
+            {
+                fs.Position = begin;
+            }
+            return result;
+        }
+
+}
+}
